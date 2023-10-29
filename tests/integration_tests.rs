8a69682048618a61030b3d0ac6031df5faea370f
Toth23 <thiagor@safe-mail.net>
2023-10-29 14:27:36 +0100

    use a random free port for the integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,20 +1,27 @@
 use vocab_crud::create_app;
 
 #[tokio::test]
-async fn test_get_endpoint() {
-    let database_url = "vocab.db".to_owned();
-    let app = create_app(database_url);
-
-    let server = axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service());
-
-    let _ = tokio::spawn(server);
+async fn health_is_ok() {
+    let port = spawn_test_server();
 
     let client = reqwest::Client::new();
-    let resp = client.get("http://localhost:3000/api/health")
+    let resp = client.get(format!("http://localhost:{port}/api/health"))
         .send()
         .await
         .unwrap();
 
     assert_eq!(resp.status(), 200);
+}
+
+fn spawn_test_server() -> u16 {
+    let database_url = "integration-test.db".to_owned();
+    let app = create_app(database_url);
+
+    let server = axum::Server::bind(&"0.0.0.0:0".parse().unwrap())
+        .serve(app.into_make_service());
+    let port = server.local_addr().port();
+
+    tokio::spawn(server);
+
+    port
 }
