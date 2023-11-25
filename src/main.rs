b6338044b8896c940e10f1e9ea4b493a2fc94574
Toth23 <thiagor@safe-mail.net>
2023-11-25 17:39:23 +0100

    read host and port from env, with default still 127.0.0.1:3000

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,15 @@ async fn main() {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in env");
     let app = create_app(database_url);
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_owned());
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_owned())
+        .parse::<u16>()
+        .expect("Port not valid");
+    let addr: SocketAddr = format!("{host}:{port}")
+        .parse()
+        .expect("Not a valid socket address");
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
